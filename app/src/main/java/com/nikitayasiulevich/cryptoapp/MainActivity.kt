ace3b11318b1a1523ced069e661826343b8c41d9
NikitaYasiulevich <yasiulevichnikita@gmail.com>
2024-06-17 20:50:30 +0300

    Implement cryptocurrency details retrieval and cyclic network requests for data updates

--- app/src/main/java/com/nikitayasiulevich/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/nikitayasiulevich/cryptoapp/MainActivity.kt
@@ -3,33 +3,18 @@ package com.nikitayasiulevich.cryptoapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.nikitayasiulevich.cryptoapp.api.ApiFactory
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC, ETH")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_OF_LOADING_DATA", it.toString())
-            }, {
-                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
-            }
-
-            )
-        compositeDisposable.addAll(disposable)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.getDetailInfo("BTC").observe(this) {
+            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        }
     }
 }
--- app/src/main/java/com/nikitayasiulevich/cryptoapp/pojo/CoinPriceInfo.kt
+++ app/src/main/java/com/nikitayasiulevich/cryptoapp/pojo/CoinPriceInfo.kt
@@ -1,156 +1,158 @@
 package com.nikitayasiulevich.cryptoapp.pojo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-
-
+@Entity(tableName = "full_price_list")
 data class CoinPriceInfo(
 @SerializedName("TYPE")
 @Expose
-private val type: String? = null,
+val type: String? = null,
 
 @SerializedName("MARKET")
 @Expose
-private val market: String? = null,
+val market: String? = null,
 
+@PrimaryKey
 @SerializedName("FROMSYMBOL")
 @Expose
-private val fromSymbol: String? = null,
+val fromSymbol: String = "",
 
 @SerializedName("TOSYMBOL")
 @Expose
-private val toSymbol: String? = null,
+val toSymbol: String? = null,
 
 @SerializedName("FLAGS")
 @Expose
-private val flags: String? = null,
+val flags: String? = null,
 
 @SerializedName("LASTMARKET")
 @Expose
-private val lastMarket: String? = null,
+val lastMarket: String? = null,
 
 @SerializedName("MEDIAN")
 @Expose
-private val median: Double? = null,
+val median: Double? = null,
 
 @SerializedName("TOPTIERVOLUME24HOUR")
 @Expose
-private val topTierVolume24hour: Double? = null,
+val topTierVolume24hour: Double? = null,
 
 @SerializedName("TOPTIERVOLUME24HOURTO")
 @Expose
-private val topTierVolume24hourto: Double? = null,
+val topTierVolume24hourto: Double? = null,
 
 @SerializedName("LASTTRADEID")
 @Expose
-private val lastTradeid: String? = null,
+val lastTradeId: String? = null,
 
 @SerializedName("PRICE")
 @Expose
-private val price: Double? = null,
+val price: Double? = null,
 
 @SerializedName("LASTUPDATE")
 @Expose
-private val lastUpdate: Int? = null,
+val lastUpdate: Int? = null,
 
 @SerializedName("LASTVOLUME")
 @Expose
-private val lastVolume: Double? = null,
+val lastVolume: Double? = null,
 
 @SerializedName("LASTVOLUMETO")
 @Expose
-private val lastVolumeto: Double? = null,
+val lastVolumeto: Double? = null,
 
 @SerializedName("VOLUMEHOUR")
 @Expose
-private val volumeHour: Double? = null,
+val volumeHour: Double? = null,
 
 @SerializedName("VOLUMEHOURTO")
 @Expose
-private val volumeHourTo: Double? = null,
+val volumeHourTo: Double? = null,
 
 @SerializedName("OPENHOUR")
 @Expose
-private val openHour: Double? = null,
+val openHour: Double? = null,
 
 @SerializedName("HIGHHOUR")
 @Expose
-private val highHour: Double? = null,
+val highHour: Double? = null,
 
 @SerializedName("LOWHOUR")
 @Expose
-private val lowHour: Double? = null,
+val lowHour: Double? = null,
 
 @SerializedName("VOLUMEDAY")
 @Expose
-private val volumeDay: Double? = null,
+val volumeDay: Double? = null,
 
 @SerializedName("VOLUMEDAYTO")
 @Expose
-private val volumeDayTo: Double? = null,
+val volumeDayTo: Double? = null,
 
 @SerializedName("OPENDAY")
 @Expose
-private val openDay: Double? = null,
+val openDay: Double? = null,
 
 @SerializedName("HIGHDAY")
 @Expose
-private val highDay: Double? = null,
+val highDay: Double? = null,
 
 @SerializedName("LOWDAY")
 @Expose
-private val lowDay: Double? = null,
+val lowDay: Double? = null,
 
 @SerializedName("VOLUME24HOUR")
 @Expose
-private val volume24hour: Double? = null,
+val volume24hour: Double? = null,
 
 @SerializedName("VOLUME24HOURTO")
 @Expose
-private val volume24hourTo: Double? = null,
+val volume24hourTo: Double? = null,
 
 @SerializedName("OPEN24HOUR")
 @Expose
-private val open24hour: Double? = null,
+val open24hour: Double? = null,
 
 @SerializedName("HIGH24HOUR")
 @Expose
-private val high24hour: Double? = null,
+val high24hour: Double? = null,
 
 @SerializedName("LOW24HOUR")
 @Expose
-private val low24hour: Double? = null,
+val low24hour: Double? = null,
 
 @SerializedName("CHANGE24HOUR")
 @Expose
-private val change24hour: Double? = null,
+val change24hour: Double? = null,
 
 @SerializedName("CHANGEPCT24HOUR")
 @Expose
-private val changepct24hour: Double? = null,
+val changepct24hour: Double? = null,
 
 @SerializedName("CHANGEDAY")
 @Expose
-private val changeday: Double? = null,
+val changeday: Double? = null,
 
 @SerializedName("CHANGEPCTDAY")
 @Expose
-private val changepctday: Double? = null,
+val changepctday: Double? = null,
 
 @SerializedName("CHANGEHOUR")
 @Expose
-private val changehour: Double? = null,
+val changehour: Double? = null,
 
 @SerializedName("CHANGEPCTHOUR")
 @Expose
-private val changepcthour: Double? = null,
+val changepcthour: Double? = null,
 
 @SerializedName("CONVERSIONTYPE")
 @Expose
-private val conversiontype: String? = null,
+val conversiontype: String? = null,
 
 @SerializedName("CONVERSIONSYMBOL")
 @Expose
-private val conversionsymbol: String? = null,
+val conversionsymbol: String? = null,
 )
