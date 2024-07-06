cb2f2256ef8d80c6f5e3bc0976b79814e8a7f114
NikitaYasiulevich <yasiulevichnikita@gmail.com>
2024-07-06 21:39:54 +0300

    Use CoinDetailFragment on CoinDetailActivity

--- app/src/main/java/com/nikitayasiulevich/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/nikitayasiulevich/cryptoapp/presentation/CoinDetailActivity.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import com.nikitayasiulevich.cryptoapp.R
 import com.nikitayasiulevich.cryptoapp.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityCoinDetailBinding.inflate(layoutInflater)
     }
-    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,18 +24,11 @@ class CoinDetailActivity : AppCompatActivity() {
             finish()
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this) {
-            with(binding) {
-                tvPrice.text = it.price.toString()
-                tvMinPrice.text = it.lowDay.toString()
-                tvMaxPrice.text = it.highDay.toString()
-                tvLastMarket.text = it.lastMarket
-                tvLastUpdate.text = it.lastUpdate
-                tvFromSymbol.text = it.fromSymbol
-                tvToSymbol.text = it.toSymbol
-                Picasso.get().load(it.imageUrl).into(ivLogoCoin)
-            }
+        if(savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 
